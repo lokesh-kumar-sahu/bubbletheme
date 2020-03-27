6ce247023289e81047004a2dadc2eb889597fedc
Lokesh <you@example.com>
2020-03-27 14:01:54 +0530

    First-commit

---
+++ assets/js/custom.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+$('.slide').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+});
+
