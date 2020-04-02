040d4c5275e7162ee66df6fcb5e44af566a7f9a2
Lokesh <you@example.com>
2020-04-02 10:32:45 +0530

    second-commit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -34,3 +34,31 @@ $('.slide').owlCarousel({
 })
 });
 
+
+$(document).ready(function(){
+   $('.slider-content').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+});
